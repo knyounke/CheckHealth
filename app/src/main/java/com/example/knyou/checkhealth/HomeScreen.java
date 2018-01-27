8e202bfb19c1f83b0e08f30ef44a2e3a33c222d3
Jenny <jnajera2@gmail.com>
2018-01-27 16:29:48 -0600

    fixed button errors

--- app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
+++ app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
@@ -3,6 +3,7 @@ package com.example.knyou.checkhealth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeScreen extends AppCompatActivity {
@@ -14,24 +15,21 @@ public class HomeScreen extends AppCompatActivity {
     }
     
     public class Enter extends AppCompatActivity {
-        ImageButton enter;
-        
-        protected void onCreate(Bundle savedInstancesState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_enter);
-            enter=(ImageButton)findViewByID(R.id.imageButton);
-            enter.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    try {
-                        connectService();
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
+       protected void onCreate(Bundle savedInstanceState) {
+           super.onCreate(savedInstanceState);
 
-        private void findViewByID(int imageButton) {
-        }
+           setContentView(R.layout.activity_enter);
+
+           final ImageButton button = findViewById(R.id.imageButton);
+           button.setOnClickListener(new View.OnClickListener() {
+
+               @Override
+               public void onClick(View view) {
+
+               }
+
+           });
+       }
+
     }
 }
