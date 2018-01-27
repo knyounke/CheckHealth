487c76925bd8ea1a08d6dfbd368b18792a0d1b57
Jenny <jnajera2@gmail.com>
2018-01-27 16:22:00 -0600

    added new menu activity. Need to fix errors with button to enter menu

--- app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
+++ app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
@@ -2,6 +2,8 @@ package com.example.knyou.checkhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+    
+    public class Enter extends AppCompatActivity {
+        ImageButton enter;
+        
+        protected void onCreate(Bundle savedInstancesState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_enter);
+            enter=(ImageButton)findViewByID(R.id.imageButton);
+            enter.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        connectService();
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        private void findViewByID(int imageButton) {
+        }
     }
 }
