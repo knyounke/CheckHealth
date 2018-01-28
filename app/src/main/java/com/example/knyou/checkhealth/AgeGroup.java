23188f15d88ddc3a5efa33ef92949f1b060c62d7
Jose Cervantes <abdn0518@gmail.com>
2018-01-28 02:23:45 -0600

    added new groups to AgeGroup

--- app/src/main/java/com/example/knyou/checkhealth/AgeGroup.java
+++ app/src/main/java/com/example/knyou/checkhealth/AgeGroup.java
@@ -10,44 +10,69 @@ public class AgeGroup {
 
     public AgeGroup(int age_group){
         if(age_group == 1){
-            checkups.add("Annual Physical Exam (Once a year)\n");
-            checkups.add("Blood Pressure (Once a year)\n");
-            checkups.add("Testicular Cancer  (Monthly Check-up)\n");
-            checkups.add("Vision Examination (Once a year)\n");
-            checkups.add("Dental Exam (Once every 6 months)\n");
-            checkups.add("Cholesterol Testing (Every 5 years)\n");
-            checkups.add("Heart Disease Check-up (Once a year)\n");
-            checkups.add("Sexually Transmitted Diseases (Once a year)\n");
-            checkups.add("Flu Vaccine (Once a year)\n");
-            checkups.add("Tetanus-diphtheria booster (Once every 10 Years)\n");
+            checkups.add(1,"Annual Physical Exam (Once a year)\n");
+            checkups.add(1,"Blood Pressure (Once a year)\n");
+            checkups.add(12,"Testicular Cancer  (Monthly Check-up)\n");
+            checkups.add(1,"Vision Examination (Once a year)\n");
+            checkups.add(2,"Dental Exam (Once every 6 months)\n");
+            checkups.add(1,"Cholesterol Testing (Every 5 years)\n");
+            checkups.add(1,"Heart Disease Check-up (Once a year)\n");
+            checkups.add(1,"Sexually Transmitted Diseases (Once a year)\n");
+            checkups.add(1,"Flu Vaccine (Once a year)\n");
+            checkups.add(1,"Tetanus-diphtheria booster (Once every 10 Years)\n");
         }
         else if(age_group == 2){
-            checkups.add("Annual Physical Exam (Once a year)");
-            checkups.add("Blood Pressure (Once a year)");
-            checkups.add("Dental Exam (Once every 6 months)");
-            checkups.add("Testicular Cancer  (Monthly Check-up)");
-            checkups.add("Cholesterol Testing (Every 5 years)");
-            checkups.add("Heart Disease Check-up (Once a year)");
-            checkups.add("Sexually Transmitted Diseases (Once a year)");
-            checkups.add("Vision Examination (Once a year)");
-            checkups.add("Coronary Heart Disease Screening (Once a year)");
-            checkups.add("Flu Vaccine (Once a year)");
-            checkups.add("Tetanus-diphtheria booster (Once every 10 Years)");
+            checkups.add(1,"Annual Physical Exam (Once a year)");
+            checkups.add(1,"Blood Pressure (Once a year)");
+            checkups.add(2,"Dental Exam (Once every 6 months)");
+            checkups.add(1,"Testicular Cancer  (Monthly Check-up)");
+            checkups.add(1,"Cholesterol Testing (Every 5 years)");
+            checkups.add(1,"Heart Disease Check-up (Once a year)");
+            checkups.add(1,"Sexually Transmitted Diseases (Once a year)");
+            checkups.add(1,"Vision Examination (Once a year)");
+            checkups.add(1,"Coronary Heart Disease Screening (Once a year)");
+            checkups.add(1,"Flu Vaccine (Once a year)");
+            checkups.add(1,"Tetanus-diphtheria booster (Once every 10 Years)");
         }
         else if(age_group ==3){
-            checkups.add("Annual Physical Exam (Once a year)");
-            checkups.add("Blood Pressure (Once a year)");
-            checkups.add("Testicular Cancer  (Monthly Check-up)");
-            checkups.add("Dental Exam (Once every 6 months)");
-            checkups.add("Cholesterol Testing (Every 5 years)");
-            checkups.add("Heart Disease Check-up (Once a year)");
-            checkups.add("Prostate Cancer (Once every 4 years)");
-            checkups.add("Diabetes Screening (Once every 3 years)");
-            checkups.add("Sexually Transmitted Diseases (Once a year)");
-            checkups.add("Vision Examination (Once a year)");
-            checkups.add("Coronary Heart Disease Screening (Once a year)");
-            checkups.add("Flu Vaccine (Once a year)");
-            checkups.add("Tetanus-diphtheria booster (Once every 10 Years))");
+            checkups.add(1,"Annual Physical Exam (Once a year)");
+            checkups.add(1,"Blood Pressure (Once a year)");
+            checkups.add(12,"Testicular Cancer  (Monthly Check-up)");
+            checkups.add(2,"Dental Exam (Once every 6 months)");
+            checkups.add(1,"Cholesterol Testing (Every 5 years)");
+            checkups.add(1,"Heart Disease Check-up (Once a year)");
+            checkups.add(1,"Prostate Cancer (Once every 4 years)");
+            checkups.add(1,"Diabetes Screening (Once every 3 years)");
+            checkups.add(1,"Sexually Transmitted Diseases (Once a year)");
+            checkups.add(1,"Vision Examination (Once a year)");
+            checkups.add(1,"Coronary Heart Disease Screening (Once a year)");
+            checkups.add(1,"Flu Vaccine (Once a year)");
+            checkups.add(1,"Tetanus-diphtheria booster (Once every 10 Years))");
+        }
+        else if(age_group == 4){
+            checkups.add(1,"Annual Physical Exam (Once a year)");
+            checkups.add(1,"Blood Pressure (Once a year)");
+            checkups.add(1,"Testicular Cancer  (Monthly Check-up)");
+            checkups.add(1,"Dental Exam (Once every 6 months)");
+            checkups.add(1,"Cholesterol Testing (Every 5 years)");
+            checkups.add(1,"Type II Diabetes Screening (Every 2 Years)");
+            checkups.add(1,"Sexually Transmitted Diseases (Once a year)" );
+            checkups.add(1,"Screening for Lipid Disorders (Once every 2-3 Years)");
+            checkups.add(1,"Electrocardiogram (Once a year)");
+            checkups.add(1,"Vision Examination (Once a year)");
+            checkups.add(1,"Hearing Examination (Once a year)");
+            checkups.add(1,"Prostate Cancer (Once every 4 years)");
+            checkups.add(1,"Colonoscopy (Every 10 Years)");
+            checkups.add(1,"CT colonography (Virtual Colonoscopy) (Every 5 years)");
+            checkups.add(1,"Flexible Sigmoidoscopy (Every 5 years)");
+            checkups.add(1,"Double-contrast Barium Enema (Every 5 years)");
+            checkups.add(1,"Fecal immunochemical test (FIT) (Once a year)");
+            checkups.add(1,"Guaiac-based fecal occult blood test (Once a year)");
+            checkups.add(1,"Stool DNA Test (Once every 3 years)");
+            checkups.add(1,"Coronary Heart Disease Screening (Once a year)");
+            checkups.add(1,"Screening for Depression (Once a year)");
+            checkups.add(1,"Flu Vaccine (Once a year)");
+            checkups.add(1,"Tetanus-diphtheria booster (Once every 10 Years)");
         }
 
     }
