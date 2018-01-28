05fe343f3b97042cf2d632a06eba2fa06c54a3a5
Jose Cervantes <abdn0518@gmail.com>
2018-01-28 07:02:48 -0600

    fixed person class

--- app/src/main/java/com/example/knyou/checkhealth/Person.java
+++ app/src/main/java/com/example/knyou/checkhealth/Person.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class Person {
     public Person(String sex, int age){
-        if(age >= 0 && (sex.equalsIgnoreCase("Male") || sex.equalsIgnoreCase("Female"))){
+        if(age >= 0){
             _age = age;
             _sex = sex;
             _set_group();
@@ -17,24 +17,50 @@ public class Person {
     }
 
     public void _set_group(){
-        if(_age < 9){
-            _age_group = new AgeGroup(0,_sex);
+        if(_sex == "Male"){
+            if(_age >=20 && _age<30){
+                _age_group = new AgeGroup(1,_sex);
+            }
+            else if(_age >=30 && _age<40){
+                _age_group = new AgeGroup(2,_sex);
+            }
+            else if(_age >=40 && _age<50){
+                _age_group = new AgeGroup(3,_sex);
+            }
+            else if(_age >=50 && _age<60){
+                _age_group = new AgeGroup(4,_sex);
+            }
+            else if(_age >=60 && _age<70){
+                _age_group = new AgeGroup(5,_sex);
+            }
+            else if(_age >=70 ){
+                _age_group = new AgeGroup(6,_sex);
+            }
         }
-        else if(_age >=20 && _age<30){
-            _age_group = new AgeGroup(1,_sex);
+        else{
+            if(_age >=18 && _age<30){
+                _age_group = new AgeGroup(7,_sex);
+            }
+            else if(_age >=40 && _age<50){
+                _age_group = new AgeGroup(8,_sex);
+            }
+            else if(_age >=50 && _age<60){
+                _age_group = new AgeGroup(9,_sex);
+            }
+            else if(_age >=60){
+                _age_group = new AgeGroup(10,_sex);
+            }
         }
-        else if(_age >=40 && _age<50){
-            _age_group = new AgeGroup(2,_sex);
-        }
-        else if(_age >=20 && _age<60){
-            _age_group = new AgeGroup(3,_sex);
-        }
-        else if(_age >=60 && _age<70){
-            _age_group = new AgeGroup(4,_sex);
-        }
-        else if(_age >=80 && _age<90){
-            _age_group = new AgeGroup(5,_sex);
-        }
+
+    }
+    public AgeGroup get_age_group(){
+        return _age_group;
+    }
+    public String get_sex(){
+        return _sex;
+    }
+    public ArrayList get_check_ups(){
+        return _check_ups;
     }
 
 
