d858e6184d91d29df5130daf7d834e261ec27242
Jose Cervantes <abdn0518@gmail.com>
2018-01-28 02:36:08 -0600

    added age_group to AgeGroup

--- app/src/main/java/com/example/knyou/checkhealth/AgeGroup.java
+++ app/src/main/java/com/example/knyou/checkhealth/AgeGroup.java
@@ -52,8 +52,8 @@ public class AgeGroup {
         else if(age_group == 4){
             checkups.add(1,"Annual Physical Exam (Once a year)");
             checkups.add(1,"Blood Pressure (Once a year)");
-            checkups.add(1,"Testicular Cancer  (Monthly Check-up)");
-            checkups.add(1,"Dental Exam (Once every 6 months)");
+            checkups.add(12,"Testicular Cancer  (Monthly Check-up)");
+            checkups.add(2,"Dental Exam (Once every 6 months)");
             checkups.add(1,"Cholesterol Testing (Every 5 years)");
             checkups.add(1,"Type II Diabetes Screening (Every 2 Years)");
             checkups.add(1,"Sexually Transmitted Diseases (Once a year)" );
@@ -73,6 +73,31 @@ public class AgeGroup {
             checkups.add(1,"Screening for Depression (Once a year)");
             checkups.add(1,"Flu Vaccine (Once a year)");
             checkups.add(1,"Tetanus-diphtheria booster (Once every 10 Years)");
+        }
+        else if(age_group == 5){
+            checkups.add("Annual Physical Exam (Once a year)");
+            checkups.add("Blood Pressure (Once a year)");
+            checkups.add("Testicular Cancer  (Monthly Check-up)");
+            checkups.add("Dental Exam (Once every 6 months)");
+            checkups.add("Prostate Cancer (Once every 4 years)");
+            checkups.add("Screening for Lipid Disorders (Once every 2-3 Years)");
+            checkups.add("Electrocardiogram (Once a year)");
+            checkups.add("Coronary Heart Disease Screening (Once a year)");
+            checkups.add("Colonoscopy (Every 10 Years)");
+            checkups.add("CT colonography (Virtual Colonoscopy) (Every 5 years)");
+            checkups.add("Flexible Sigmoidoscopy (Every 5 years)");
+            checkups.add("Double-contrast Barium Enema (Every 5 years)");
+            checkups.add("Fecal immunochemical test (FIT) (Once a year)");
+            checkups.add("Guaiac-based fecal occult blood test (Once a year)");
+            checkups.add("Stool DNA Test (Once every 3 years)");
+            checkups.add("Vision Examination (Once a year)");
+            checkups.add("Hearing Examination (Once a year)");
+            checkups.add("Osteoporosis Screening  (Once every 8 years)");
+            checkups.add("Sexually Transmitted Diseases (Once a year)");
+            checkups.add("Screening for Depression (Once a year)");
+            checkups.add("Alzheimer's disease and Dementia (Once every 5 years)");
+            checkups.add("Flu Vaccine (Once a year)");
+            checkups.add("Tetanus-diphtheria booster (Once every 10 Years)");
         }
 
     }
