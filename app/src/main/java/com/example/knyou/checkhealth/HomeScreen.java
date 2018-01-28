bd940855d94ac380a3f0bb96b4780b3abb1f95c1
Jenny <jnajera2@gmail.com>
2018-01-27 23:43:09 -0600

    updating the MenuActivity xml files, and other changes

--- app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
+++ app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
@@ -12,19 +12,15 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
-    
-    public class MenuActivity extends AppCompatActivity {
-       protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
 
-           setContentView(R.layout.activity_menu);
+        //connect to button
+        setContentView(R.layout.activity_menu);
 
-           final ImageButton button = findViewById(R.id.imageButton);
-           button.setOnClickListener(new View.OnClickListener() {
+        final ImageButton button = findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
 
-               @Override
-               public void onClick(View view) {
+            @Override
+            public void onClick(View view) {
 
                }
 
@@ -32,4 +28,4 @@ public class HomeScreen extends AppCompatActivity {
        }
 
     }
-}
+
--- app/src/main/java/com/example/knyou/checkhealth/MenuActivity.java
+++ app/src/main/java/com/example/knyou/checkhealth/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.knyou.checkhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.widget.DatePicker;
+import android.widget.TextView;
 import java.util.Calendar;
 
 public class MenuActivity extends AppCompatActivity {
@@ -11,5 +13,30 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        
+        //calendar
+        Calendar c= Calendar.getInstance();
+        int year= c.get(c.YEAR);
+        int month = c.get(c.MONTH);
+        int dayOfMonth = c.get(c.DAY_OF_MONTH);
+        
+        //get widgets reference of calendar
+
+        DatePicker BDay = (DatePicker)findViewById(R.id.BDay);
+        
+        //init(int year, int monthofyear, int dayofmonth, datepicker.ondatelistener
+        
+        /*BDay.init{
+            year,
+            month,
+            dayOfMonth,
+            new DatePicker.OnDateChangedListener() {
+           @Override
+           public void onDateChanged() {
+
+           }
+            };
+
+        }*/
     }
 }
