4aa627626c76895aaf76c15789d3db47194008b1
knyou <knyounke@gmail.com>
2018-01-27 18:35:26 -0600

    Added working notifications feature with instructions on how to call it in other portions of the app.

--- app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
+++ app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
@@ -3,13 +3,7 @@ package com.example.knyou.checkhealth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-        //test commit and push
-    }
+
 }
-//this is java
