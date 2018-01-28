256fc9a7f6bb309cad720171eb0f1cd98093e736
Jose Cervantes <abdn0518@gmail.com>
2018-01-28 06:10:54 -0600

    //

--- app/src/main/java/com/example/knyou/checkhealth/Person.java
+++ app/src/main/java/com/example/knyou/checkhealth/Person.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
  * Created by jose on 1/27/18.
  */
 
-public class Person { /*
+public class Person {
+    /*
     public Person(String sex, int age){
         if(age >= 0 && (sex.equalsIgnoreCase("Male") || sex.equalsIgnoreCase("Female"))){
             _age = age;
