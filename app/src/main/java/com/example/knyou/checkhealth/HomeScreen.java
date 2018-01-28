a136f24a7bc630a814d4b6b393f0c542164784de
knyou <knyounke@gmail.com>
2018-01-28 04:31:21 -0600

    Resolved all compiling and build errors.

--- app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
+++ app/src/main/java/com/example/knyou/checkhealth/HomeScreen.java
@@ -1,30 +1,13 @@
 package com.example.knyou.checkhealth;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-public class HomeScreen extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-
-        //connect to menu button
-        setContentView(R.layout.activity_menu);
-
-        final ImageButton button = findViewById(R.id.imageButton);
-        button.setOnClickListener(new View.OnClickListener() {
+         import android.support.v7.app.AppCompatActivity;
+         import android.os.Bundle;
+         
+         public class HomeScreen extends AppCompatActivity {
 
             @Override
-            public void onClick(View view) {
-
-               }
-
-           });
-       }
-
-    }
+     protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_home_screen);
+          }
+ }
