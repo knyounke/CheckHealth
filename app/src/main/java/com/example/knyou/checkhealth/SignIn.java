c7a77744217476405dbc38bfb1581eaeccf6ee59
knyou <knyounke@gmail.com>
2018-01-31 18:27:43 -0600

    Working on Authentification feature in SignIn Activity.

---
+++ app/src/main/java/com/example/knyou/checkhealth/SignIn.java
@@ -0,0 +1,25 @@
+package com.example.knyou.checkhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        private FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+}
